5ed31db770a54a568ed31506290852132d528dcf
Alex-Stupak <stupakalexx@gmail.com>
2021-12-06 13:51:06 +0200

    rework

--- src/index.js
+++ src/index.js
@@ -1,55 +1,47 @@
 import './css/styles.css';
-import 'reset-css';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
-import { Notify } from 'notiflix';
+import getRefs from './js/get-refs';
+import Notiflix from 'notiflix';
+import SearchCountryService from './js/fetchCountries'
+import countriesCardTpl from './templates/country-card.hbs'
+import countriesItemTpl from './templates/countries-item.hbs'
+
 const DEBOUNCE_DELAY = 300;
-const searchBox = document.getElementById('search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-const handleInput = event => {
-const search = event.target.value.trim();
-if (!search.length) {
-cleanList();
-return;
+const refs = getRefs()
+const searchCountryService = new SearchCountryService();
+
+refs.searchInput.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY))
+
+function onInputChange(element) {
+    removeRender();
+
+    searchCountryService.query = element.target.value.trim();
+
+    if (searchCountryService.query === '') {
+        return Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+    
+    searchCountryService.fetchCountries()
+        .then(countries => {
+            if (!countries) {
+                return
+            }
+            renderCountry(countries)
+        })
+        .catch(error => {
+            console.log(error)
+        })
 }
-fetchCountries(search)
-.then(data => {
-cleanList();
-onDataReceived(data);
-})
-.catch(error => {
-console.error(error);
-cleanList();
-Notify.failure('Oops there is no country with that name');
-});
-};
-searchBox.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
-const onDataReceived = array => {
-if (array.length > 10) {
-Notify.info('Too many matches found. Please enter a more specific name.');
-} else if (array.length > 1 && array.length <= 10) {
-renderList(array);
-} else if (array.length === 1) {
-renderCountry(array[0]);
+
+function renderCountry(countries) {
+    if (countries.length === 1) {
+        return refs.countryCard.insertAdjacentHTML('afterbegin', countriesCardTpl(countries))
+    }
+    
+     refs.countryList.insertAdjacentHTML('afterbegin', countriesItemTpl(countries))
 }
-};
-const renderList = array => {
-const elements = array
-.map(({ name, flag }) => {
-return `<li id="country_item"> <img class="flag" src=${flag}> ${name}</li>`;
-})
-.join('');
-countryList.insertAdjacentHTML('beforeend', elements);
-};
-const renderCountry = ({ name, flag, population, languages, capital }) => {
-const markup = `<p><h2 class="text text_two"><img class="flag" src=${flag}> ${name}</h2></p>
-<p class="data"><span class="text text_data">Capital:</span> ${capital}</p>
-<p class="data"><span class="text text_data">Population:</span> ${population}</p>
-<p class="data"><span class="text text_data">Languages:</span> ${languages[0].name}</p>`;
-countryInfo.insertAdjacentHTML('beforeend', markup);
-};
-const cleanList = () => {
-countryList.innerHTML = '';
-countryInfo.innerHTML = '';
-};
+
+function removeRender() {
+    refs.countryCard.innerHTML = '';
+    refs.countryList.innerHTML = '';
+}
